95ee3e9d36dbb7c499508ae6dd8bfb5ac3a409e8
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-26 19:07:05 +0600

    updated

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,17 +1,55 @@
-import React, { use } from 'react';
+import React, { use, useState } from "react";
 
-const Users = ({usersPromise}) => {
+const Users = ({ usersPromise }) => {
+  const initialUsers = use(usersPromise);
 
-    const users = use(usersPromise);
-    console.log(users);
-    return (
-        <div>
-            <h3>Users data</h3>
-            {
-                users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
-            }
-        </div>
-    );
+  const [users, setUsers] = useState(initialUsers);
+  console.log(users);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+
+    const user = {name, email};
+
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('data after post', data)
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        e.target.reset();
+    })
+
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Name: </label>
+        <input name="name" type="text" />
+        <br />
+        <label htmlFor="">Email: </label>
+        <input name="email" type="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <h3>Users data</h3>
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.name} {user.email}
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
